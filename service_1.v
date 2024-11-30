0ac6dd586ad83e2e375047a8bf09b9ee92fc05d5
yuhyojeong <128355344+yuhyojeong@users.noreply.github.com>
2024-11-30 21:21:40 +0900

    Update service_1.v

--- service_1.v
+++ service_1.v
@@ -27,38 +27,37 @@ module Service_1_time_set (
     input push_l,
     input push_r,
 
-    output [3:0] an, // one-hot style, 1 if segment in that location is on
+    output reg [3:0] sel,
     output reg finish1,
     output reg [15:0] num // 15:12 11:8 7:4 3:0 = min min sec sec, each 4bit 0-9
 );
 
   reg [1:0] seg; // 3 2 1 0 left to right
-  reg [3:0] rev_an;
 
   // select segment
   always @(posedge clk) begin
     if (!resetn) begin
       seg <= 0;
-      rev_an <= 0;
+      sel <= 0;
     end
     else begin
       if (spdt1) begin
-        if (rev_an == 0) begin
-          rev_an <= 4'b1000; // init
+        if (sel == 0) begin
+          sel <= 4'b1000; // init
           seg <= 3;
         end
         else begin
           if (push_l) begin
             seg <= seg + 1;
-            rev_an <= (rev_an == 4'b1000) ? 4'b0001 : rev_an << 1;
+            sel <= (sel == 4'b1000) ? 4'b0001 : sel << 1;
           end
           else if (push_r) begin
             seg <= seg - 1;
-            rev_an <= (rev_an == 4'b0001) ? 4'b1000 : rev_an >> 1;
+            sel <= (sel == 4'b0001) ? 4'b1000 : sel >> 1;
           end
         end
       end
-      if (finish1) rev_an <= 4'b1111;
+      if (finish1) sel <= 4'b1111;
     end
   end
 
@@ -79,10 +78,7 @@ module Service_1_time_set (
   // finish
   always @(posedge clk) begin
     if (!resetn) finish1 <= 0;
-    else if (!spdt1 & rev_an) finish1 <= 1;
+    else if (!spdt1 & sel) finish1 <= 1;
   end
-
-  assign an = (rev_an == 0) ? 0 : ~rev_an;
-
 endmodule
 
