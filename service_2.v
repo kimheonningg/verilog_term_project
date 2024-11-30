dd951708b0da3f72dafd5986cb5df820cfc75495
yuhyojeong <128355344+yuhyojeong@users.noreply.github.com>
2024-11-30 21:33:31 +0900

    Update service_2.v

--- service_2.v
+++ service_2.v
@@ -27,38 +27,37 @@ module Service_2_alarm_set (
     input push_l,
     input push_r,
 
-    output [3:0] an,
+    output reg [3:0] sel,
     output reg finish2,
     output reg [15:0] alarm // 15:12 11:8 7:4 3:0 = min min sec sec, each 4bit 0-9
 );
 
   reg [1:0] seg; // 3 2 1 0 left to right
-  reg [3:0] rev_an;
 
   // select segment
   always @(posedge clk) begin
     if (!resetn) begin
       seg <= 0;
-      rev_an <= 0;
+      sel <= 0;
     end
     else begin
       if (spdt2) begin
-        if (rev_an == 0) begin
-          rev_an <= 4'b1000; // init
+        if (sel == 0) begin
+          sel <= 4'b1000; // init
           seg <= 3;
         end
         else begin
           if (push_l) begin
             seg <= seg + 1;
-            rev_an <= (rev_an == 4'b1000) ? 4'b0001 : rev_an << 1;
+            sel <= (sel == 4'b1000) ? 4'b0001 : sel << 1;
           end
           else if (push_r) begin
             seg <= seg - 1;
-            rev_an <= (rev_an == 4'b0001) ? 4'b1000 : rev_an >> 1;
+            sel <= (sel == 4'b0001) ? 4'b1000 : sel >> 1;
           end
         end
       end
-      if (finish2) rev_an <= 4'b1111;
+      if (finish2) sel <= 4'b1111;
     end
   end
 
@@ -81,10 +80,7 @@ module Service_2_alarm_set (
   // finish
   always @(posedge clk) begin
     if (!resetn) finish2 <= 0;
-    else if (!spdt2 & rev_an) finish2 <= 1;
+    else if (!spdt2 & sel) finish2 <= 1;
   end
-
-  assign an = (rev_an == 0) ? 0 : ~rev_an;
-
 endmodule
 
